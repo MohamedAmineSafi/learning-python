ce91b5953d9af32c53bc89fe68e0b27892d7ceb9
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-12 20:54:56 +0100

    Some data types

---
+++ 101/dic.py
@@ -0,0 +1,9 @@
+me = {
+    "name": "med",
+    "age": 18,
+    "IQ": 0
+}
+
+me["personality"] = "Lovely"
+
+print(me["personality"])
--- 101/math.py
+++ 101/math.py
@@ -1,5 +1,7 @@
-print(1+1)
-print(2*3)
-print(10/5)
-print(100%6)
-print("I hate Math")
+# print(1+1)
+# print(2*3)
+# print(10/5)
+# print(100%6)
+# print("I hate Math")
+
+print("Na" * 10)

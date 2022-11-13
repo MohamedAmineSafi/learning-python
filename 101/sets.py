1d8472a644ad8d700ddae4aa32ba07f0fdf6a22b
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-13 08:00:50 +0100

    More Data Types

---
+++ 101/sets.py
@@ -0,0 +1,7 @@
+s = {"item 1", "item 2"}
+
+print(s)
+
+s.add("item 3")
+
+print(s)
---
+++ 101/user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+
+# print("Hello,", name)
+
+gList = ["Apples", "Banana", "Oranges", "Apples"]
+
+print(tuple(gList))

b98c7823404f34c2cdc01bd4b8f463d979d36a83
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-16 21:10:10 +0100

    ReadingMultipleLines(CreatingEmailList)

---
+++ 201/readingLinesFromFiles.py
@@ -0,0 +1,12 @@
+# Creating big email list
+# with open("201/email.txt", 'w') as file:
+#     def something():
+#         i=1
+#         content = ''
+#         while i<=100:
+#             content = content + f"email{i}@gmail.com\n"
+#             i = i + 1
+#         return content
+    
+#     file.write(something())
+

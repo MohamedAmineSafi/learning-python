02438de0bdfd406dab5c6ec550bb153472eb7482
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-16 21:56:06 +0100

    exeUserFiles2

--- 201/exeUserFiles.py
+++ 201/exeUserFiles.py
@@ -3,3 +3,8 @@ content = input("What is the content? ")
 
 with open(f"201/UserFiles/{filename}.txt", 'w') as file:
     file.write(content)
+
+open_file = input("Would you like to read this file?")
+if open_file == "yes":
+    with open(f"201/UserFiles/{filename}.txt", 'r') as file:
+        print(file.read())

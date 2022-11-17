f2795b07233abcb620824e208aeae146f96c3ba9
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-17 07:38:35 +0100

    GetReq (Starwars API)

--- 201/simpleAPIReq.py
+++ 201/simpleAPIReq.py
@@ -3,4 +3,8 @@ import requests # Run <<pip install requests>>
 req = requests.get("https://swapi.dev/api/people/1")
 person = req.json()
 
-print(person['name'])
+print(f'{person["name"]} is present in these films:')
+for filmURL in person["films"]:
+    req = requests.get(filmURL)
+    filmData = req.json()
+    print(f"\t {filmData['title']}")

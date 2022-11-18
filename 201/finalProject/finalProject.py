c32658ced07a119969b9f1a69f2f79717dbefff3
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-18 20:51:25 +0100

    Final Project 2

--- 201/finalProject/finalProject.py
+++ 201/finalProject/finalProject.py
@@ -1,10 +1,19 @@
 import requests
 
-# pokemonInput = input("Choose a Pokimon: ") #ditto
-# url = f"https://pokeapi.co/api/v2/pokemon/{pokemonInput}"
-url = f"https://pokeapi.co/api/v2/pokemon/ditto"
+while True:
+    pokemonInput = input("Choose a Pokimon: ") #ditto
+    pokemonInput = pokemonInput.lower()
+    if pokemonInput == "stop":
+        break
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemonInput}"
 
-req = requests.get(url)
-GetResponse = req.json()
-print("Has an ability of", GetResponse['abilities'][0]['ability']['name'])
-print("Has an ability of", GetResponse['abilities'][1]['ability']['name'])
+    req = requests.get(url)
+    if req.status_code == 200:
+        GetResponse = req.json()
+        for ability in GetResponse['abilities']:
+            getAbilityName = ability['ability']['name']
+            print("Has an ability of", getAbilityName)
+        # print("Has an ability of", GetResponse['abilities'][0]['ability']['name'])
+        # print("Has an ability of", GetResponse['abilities'][1]['ability']['name'])
+    else:
+        print("Pokimon not found")

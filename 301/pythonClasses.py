b1e8650c0f69533e5c28eefdc8ecb0b8082b2c03
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-20 08:31:21 +0100

    Python Class 4

--- 301/pythonClasses.py
+++ 301/pythonClasses.py
@@ -5,19 +5,20 @@
 # animal = ThisIsAnAnimal()
 
 # Example
-class Animal:
+class myClass:
     this_is_a_property = ["Something", "Something Else"]
 
     def thisIsAMethod(self):
         print(self.this_is_a_property)
     
+    def addName(self, name):
+        self.this_is_a_property.append(name)
+        return self.this_is_a_property
+
     @property
     def myMethod(self):
-        # self.this_is_a_property.insert("Med Amine is the best")
         return self.this_is_a_property[1]
 
-the_animal = Animal()
-print(the_animal.this_is_a_property)
-the_animal.thisIsAMethod()
-new = the_animal.myMethod
-print(new)
+newClass = myClass()
+print(newClass.this_is_a_property)
+print(newClass.addName("Meedou"))

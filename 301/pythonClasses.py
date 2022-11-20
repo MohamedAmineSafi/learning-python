07b3e9ba8e095cb68d0cfb069cfd2331c5109d3b
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-20 08:26:47 +0100

    Python Class 3

--- 301/pythonClasses.py
+++ 301/pythonClasses.py
@@ -8,5 +8,16 @@
 class Animal:
     this_is_a_property = ["Something", "Something Else"]
 
+    def thisIsAMethod(self):
+        print(self.this_is_a_property)
+    
+    @property
+    def myMethod(self):
+        # self.this_is_a_property.insert("Med Amine is the best")
+        return self.this_is_a_property[1]
+
 the_animal = Animal()
-print(the_animal.this_is_a_property[1])
+print(the_animal.this_is_a_property)
+the_animal.thisIsAMethod()
+new = the_animal.myMethod
+print(new)

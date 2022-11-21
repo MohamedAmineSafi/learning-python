6ba67bca960b645a4d55734f298cf88837f5f6ff
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-21 13:25:06 +0100

    Python Class Super()

--- 301/pythonClasses.py
+++ 301/pythonClasses.py
@@ -1,30 +1,12 @@
-# This is the syntax
-# class ThisIsAnAnimal:
-#     pass
+class Animal:
+    def chase(self, animal="Mouse"):
+        return animal
 
-# animal = ThisIsAnAnimal()
+class Dog(Animal):
+    def chaseMyDog(self, animal):
+        animal = super().chase(animal)
+        print("I am chasing a", animal)
 
-# Example
-class MyClass:
-    this_is_a_property = ["Something", "Something Else"]
 
-    def thisIsAMethod(self):
-        print(self.this_is_a_property)
-    
-    def addName(self, name):
-        self.this_is_a_property.append(name)
-        return self.this_is_a_property
-
-    @property
-    def myMethod(self):
-        return self.this_is_a_property[1]
-
-newClass = MyClass()
-print(newClass.this_is_a_property)
-print(newClass.addName("Meedou"))
-
-class IAmTheBest(MyClass):
-    pass
-
-bestClass = IAmTheBest.this_is_a_property
-print("Second class", bestClass)
+dog = Dog()
+dog.chaseMyDog("Cat")

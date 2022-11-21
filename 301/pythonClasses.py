cfed6e820013e903580ea0781cc0c1018f917986
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-21 13:52:49 +0100

    Python Class Cat

--- 301/pythonClasses.py
+++ 301/pythonClasses.py
@@ -1,12 +1,27 @@
 class Animal:
-    def chase(self, animal="Mouse"):
-        return animal
+    type = None
+    color = None
+    size = None
+    age = None
+    def __init__(self):
+        print("Class Animal Activated!")
+    def chase(self, animal=None):
+        return(f"I am chasing a {animal}")
+    def talk(self, sound=None):
+        return(sound)
 
-class Dog(Animal):
-    def chaseMyDog(self, animal):
-        animal = super().chase(animal)
-        print("I am chasing a", animal)
+class Cat(Animal):
+    def __init__(self, color, size, age):
+        super().__init__()
+        print("Class Cat Activated!")
+        self.type = "Cat"
+        self.color = color
+        self.size = size
+        self.age = age
+    def summery(self):
+        print(f"I am a {self.size} {self.color} {self.type}. I am {self.age} years old!")
 
-
-dog = Dog()
-dog.chaseMyDog("Cat")
+myAnimal = Cat("White", "Big", 7)
+myAnimal.summery()
+print( myAnimal.chase("Mouse") )
+print( myAnimal.talk("Meow") )

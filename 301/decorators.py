ae5ef01f3fe00c11aab43c913cd30e7796859cc1
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-21 15:17:04 +0100

    Decorator

---
+++ 301/decorators.py
@@ -0,0 +1,12 @@
+def myDecorator(func):
+    def wrapper():
+        print("Do something")
+        func()
+        print("Something Else")
+    return wrapper
+
+@myDecorator
+def sayHi():
+    print("Hi")
+
+sayHi()
